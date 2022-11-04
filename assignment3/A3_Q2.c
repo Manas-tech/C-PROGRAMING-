bf4a573d44f83a72323d7ca5029075bcd6c5c156
manas parwani _TECHNONJR <115521778+Manas-tech@users.noreply.github.com>
2022-11-04 20:17:52 +0530

    Add files via upload

---
+++ assignment3/A3_Q2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a;
+	int b;
+	printf("FIRST VARIABLE\n");
+	scanf("%d",&a);
+	printf("VALUE OF FIRST VARIABLE IS %d \n",a);
+	printf(" SECOND VARIABLE\n");
+	scanf("%d",&b);
+	printf(" VALUE OF SECOND VARIABLE IS %d\n",b);
+	printf("THANK YOU !!!!!");
+	
+	return 0;
+}
